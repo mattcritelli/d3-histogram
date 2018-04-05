eee84b6ca15b9b79e2e98a7541a760912b0d34c5
Matt C <matthew.critelli@gmail.com>
2018-04-05 12:08:57 -0600

    Refactor to wrap main code inside a function.

--- app.js
+++ app.js
@@ -3,25 +3,7 @@ var height = 600;
 var padding = 50;
 var barPadding = 1;
 var filteredData = regionData.filter(d => d.medianAge !== null);
-
-// Create X axis scale based on median ages in filteredData
-var xScale = d3.scaleLinear()
-               .domain(d3.extent(filteredData, d => d.medianAge))
-               .range([padding, width - padding]);
-
-// Create histogram variable which stores returned function
-var histogram = d3.histogram()
-                  .domain(xScale.domain())
-                  .thresholds(xScale.ticks())
-                  .value(d => d.medianAge);
-
-// Create bins variabe to store array returned from histogram function
-var bins = histogram(filteredData)
-
-// Create Y scale based on frequency of median ages in each bin.
-var yScale = d3.scaleLinear()
-               .domain([0, d3.max(bins, d => d.length)])
-               .range([height - padding, padding]);
+var initialBinCount = 16;
 
 /**
  * Create selection of "bar" and pass in bins data.
@@ -32,34 +14,16 @@ var svg = d3.select("svg")
               .attr("width", width)
               .attr("height", height);
 
-
-svg
-  .selectAll("rect")
-  .data(bins)
-  .enter()
-  .append("rect")
-    .attr("x", d => xScale(d.x0))
-    .attr("y", d => yScale(d.length))
-    .attr("height", d => height - yScale(d.length) - padding)
-    .attr("width", d => xScale(d.x1) - xScale(d.x0) - barPadding )
-    .attr("fill", "#9c27b0");
-
-// Create X and Y Axes
-var xAxis = d3.axisBottom(xScale)
-var yAxis = d3.axisLeft(yScale)
-
 // Format X & Y Axes
 d3.select("svg")
   .append("g")
     .attr("transform", "translate(0," + (height - padding) + ")")
     .classed("x-axis", true)
-    .call(xAxis);
 
 d3.select("svg")
   .append("g")
     .attr("transform", "translate(" + padding + ",0)")
     .classed("y-axis", true)
-    .call(yAxis);
 
 // Add axes labels
 d3.select("svg")
@@ -80,32 +44,56 @@ d3.select("svg")
     .text("Frequency")
 
 d3.select("input")
-  .property("value", bins.length)
+  .property("value", initialBinCount)
   .on("input", function(){
-    var numBins = +d3.event.target.value;
-    histogram.thresholds(xScale.ticks(numBins));
-    bins = histogram(filteredData);
-    yScale.domain([0, d3.max(bins, d => d.length)]);
+    updateRects(+d3.event.target.value);
+  })
+
+updateRects(initialBinCount)
+
+function updateRects(val) {
+  // Create X axis scale based on median ages in filteredData
+  var xScale = d3.scaleLinear()
+                 .domain(d3.extent(filteredData, d => d.medianAge))
+                 .range([padding, width - padding]);
+
+  // Create histogram variable which stores returned function
+  var histogram = d3.histogram()
+                    .domain(xScale.domain())
+                    .thresholds(xScale.ticks(val))
+                    .value(d => d.medianAge);
+
+  // Create bins variabe to store array returned from histogram function
+  var bins = histogram(filteredData)
+
+  // Create Y scale based on frequency of median ages in each bin.
+  var yScale = d3.scaleLinear()
+                  .domain([0, d3.max(bins, d => d.length)])
+                  .range([height - padding, padding]);
+
+  // Create X and Y Axes
+  var xAxis = d3.axisBottom(xScale);
+  var yAxis = d3.axisLeft(yScale);
 
   d3.select(".y-axis")
-      .call(d3.axisLeft(yScale))
+    .call(d3.axisLeft(yScale));
 
   d3.select(".x-axis")
       .call(d3.axisBottom(xScale)
-              .ticks(numBins))
+              .ticks(val))
     .selectAll("text")
       .attr("x", 10)
       .attr("y", -3)
       .attr("transform", "rotate(90)")
-      .attr("text-anchor", "start")
+      .attr("text-anchor", "start");
 
   var rect = svg
-                .selectAll("rect")
-                .data(bins);
+               .selectAll("rect")
+               .data(bins);
 
   rect
     .exit()
-    .remove()
+    .remove();
 
   rect
     .enter()
@@ -121,6 +109,6 @@ d3.select("input")
       .attr("fill", "#9c27b0");
 
   d3.select(".bin-count")
-      .text(`Number of bins: ${bins.length}`)
+      .text(`Number of bins: ${bins.length}`);
 
-})
+}
